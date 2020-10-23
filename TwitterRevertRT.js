eb14e896aacb0fdf3a71dd2ac39c58679daed479
tsukumijima <tsukumijima@users.noreply.github.com>
2020-10-24 00:29:14 +0900

    イベントが被ったときに何回も処理が走らないようにした

--- TwitterRevertRT.js
+++ TwitterRevertRT.js
@@ -26,11 +26,15 @@ window.onload = function() {
             }
         });
 
+        // 連続してツイートボタンが押されるのを防ぐフラグ
+        let isClickedTweetButton = false;
+
         // 監視ターゲットの取得
         const observeItem = document.querySelector('div#layers');
+        const observeItem_sp = document.querySelector('div[data-at-shortcutkeys]');  // スマホ画面用
 
         // オブザーバーの作成
-        const observer = new MutationObserver(records => {
+        function observe(records) {
 
             for (record of records) {
 
@@ -38,39 +42,57 @@ window.onload = function() {
                 // モーダルが開くまで少しだけ待つ
                 setTimeout(function() {
 
-                    //　ツイートボタンを取得
-                    const tweetButton = record['target'].querySelector('div[data-testid="tweetButton"]');
-
                     // 右クリックからは除外
                     if (isRightClick) {
                         isRightClick = false; // フラグを降ろす
                         return;
                     }
 
-                    // ツイートボタンがが存在する & 無効化されていない
-                    if (tweetButton !== null && tweetButton.getAttribute('disabled') === null) {
+                    // モーダルがリツイート状態か
+                    // TODO: 最初にうまくいかないのは isRetweet が false になるから
+                    const isRetweet = (
+                        record['target'].querySelector('div[data-testid="attachments"] > div > div > span') &&
+                        record['target'].querySelector('div[data-testid="attachments"] > div > div > span').textContent &&
+                        record['target'].querySelector('div[data-testid="attachments"] > div > div > span').textContent === '引用ツイート'
+                    );
 
-                        // モーダルがリツイート状態か
-                        const isRetweet = (
-                            record['target'].querySelector('div[data-testid="attachments"] > div > div > span') &&
-                            record['target'].querySelector('div[data-testid="attachments"] > div > div > span').textContent &&
-                            record['target'].querySelector('div[data-testid="attachments"] > div > div > span').textContent === '引用ツイート'
-                        );
+                    //　ツイートボタンを取得
+                    const tweetButton = record['target'].querySelector('div[data-testid="tweetButton"]');
 
-                        // モーダルがリツイート状態
-                        if (isRetweet) {
+                    // モーダルがリツイート状態 & フラグがオフ
+                    if (isRetweet && isClickedTweetButton === false) {
+
+                        console.log('モーダルがリツイート状態 & フラグがオフ')
+
+                        // ツイートボタンがが存在する & 無効化されていない
+                        if (tweetButton !== null && tweetButton.getAttribute('disabled') === null) {
+
+                            console.log('ツイート')
 
                             // ツイートボタンをクリック
                             tweetButton.click();
+
+                            // フラグ管理
+                            isClickedTweetButton = true;
+                            setTimeout(function() {
+                                isClickedTweetButton = false;
+                            }, 1000);  // 1秒後にオフ
+
                         }
                     }
 
                 }, 100);
             }
-        });
+        }
+
+        const observer = new MutationObserver(observe);
+        const observer_sp = new MutationObserver(observe);
 
         // 監視の開始
         observer.observe(observeItem, {
+            childList: true,  // 子要素を監視
+        });
+        observer_sp.observe(observeItem_sp, {
             childList: true,  // 子要素を監視
         });
 
